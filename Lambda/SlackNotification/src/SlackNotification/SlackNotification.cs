c3973f9dc458d6769b366e85bc3fac5fb4a5e0cb
Jonatan Jaworski <alturil@gmail.com>
2023-04-23 00:07:16 +1200

    Update lambda

--- Lambda/SlackNotification/src/SlackNotification/SlackNotification.cs
+++ Lambda/SlackNotification/src/SlackNotification/SlackNotification.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using System.Configuration;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,6 +8,23 @@ namespace SlackNotification;
 
 public class SlackNotification
 {
+    private SlackClient _slackClient;
+
+    public SlackNotification()
+    {
+        var slackuri = ConfigurationManager.AppSettings["slackUri"];
+        if (slackuri == null)
+        {
+            throw new ArgumentNullException(nameof(slackuri));
+        }
+
+        _slackClient = new SlackClient(new Uri(slackuri!)); ;
+    }
+
+    public SlackNotification(SlackClient slackClient)
+    {
+        _slackClient = slackClient;
+    }
 
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
@@ -14,9 +32,9 @@ public class SlackNotification
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string SendSlackNotification(string input, ILambdaContext context)
+    public async Task<HttpResponseMessage> SendSlackNotification(string input, ILambdaContext context)
     {
         context.Logger.Log($"Hello from Lambda! - {input}");
-        return input;
+        return await _slackClient.SendTapEventAsync("Alturil");
     }
 }
