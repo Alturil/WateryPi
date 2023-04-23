894a9afe5f530de796c40908c0c608a3c52bc602
Jonatan Jaworski <alturil@gmail.com>
2023-04-23 22:42:22 +1200

    Add SSM, ditch env

---
+++ Lambda/SlackNotification/test/SlackNotification.Tests/AwsParameterStoreClientTests.cs
@@ -0,0 +1,64 @@
+﻿using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SlackNotification.Tests;
+
+public class AwsParameterStoreClientTests
+{
+    [Fact]
+    public async Task When_ParameterExists_Then_ReturnsValue()
+    {
+        // Arrange
+        var mockResponse = new GetParameterResponse
+        {
+            HttpStatusCode = HttpStatusCode.OK,
+            Parameter = new Parameter
+            {
+                Value = "Bar"
+            }
+        };
+
+        var mockClient = GetMockClient(mockResponse);
+        var awsParameterStoreClient = new AwsParameterStoreClient(mockClient.Object);
+
+        // Act
+        var secretValue = await awsParameterStoreClient.GetValueAsync("Foo");
+
+        // Assert
+        Assert.Equal("Bar", secretValue);
+    }
+
+    [Fact]
+    public async Task When_ParameterCantBeRetrieved_Then_ThrowsException()
+    {
+        // Arrange
+        var mockResponse = new GetParameterResponse
+        {
+            HttpStatusCode = HttpStatusCode.NotFound,            
+        };
+
+        var mockClient = GetMockClient(mockResponse);
+        var awsParameterStoreClient = new AwsParameterStoreClient(mockClient.Object);
+
+        // Act and Assert
+        await Assert.ThrowsAsync<Exception>(async ()=>
+            await awsParameterStoreClient.GetValueAsync("Foo"));
+    }
+
+    private static Mock<IAmazonSimpleSystemsManagement> GetMockClient(GetParameterResponse mockResponse)
+    {
+        var mockClient = new Mock<IAmazonSimpleSystemsManagement>();
+        mockClient.Setup(c => c.GetParameterAsync(It.IsAny<GetParameterRequest>(), CancellationToken.None))
+            .ReturnsAsync(mockResponse);
+
+        return mockClient;
+    }
+}
